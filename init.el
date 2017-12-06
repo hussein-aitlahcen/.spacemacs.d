cb1e5c9a3626d274b6b69dc5bd213d3333c16344
Hussein Ait Lahcen <hussein.aitlahcen@cgi.com>
2017-12-06 13:17:29 +0000

    Add: Whitespace character mapping

--- init.el
+++ init.el
@@ -160,12 +160,16 @@
   (load-file "~/.spacemacs.d/pretty-magit.el")
   (load-file "~/.spacemacs.d/pretty-fonts.el")
   (global-whitespace-mode t)
-  (set-face-foreground 'whitespace-newline "#636363")
-  (set-face-foreground 'whitespace-tab "#636363")
+  (set-face-foreground 'whitespace-newline "#454545")
+  (set-face-foreground 'whitespace-tab "#454545")
   (set-face-background 'whitespace-tab 'nil)
   (set-face-underline  'whitespace-tab t)
-  (set-face-foreground 'whitespace-space "#636363")
+  (set-face-foreground 'whitespace-space "#454545")
   (set-face-background 'whitespace-space 'nil)
+  (setq whitespace-display-mappings
+        '((newline-mark 10 [182 10])
+          (space-mark 32 [183] [46])
+          (tab-mark 9 [9655 9] [92 9])))
   (global-pretty-mode t)
   (pretty-deactivate-groups
    '(:equality :ordering :ordering-double :ordering-triple
