61bb1827c2043e69e3de949cd71a68f95e6d4333
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-06-18 17:21:47 +0200

    fix: autocommit

--- init.el
+++ init.el
@@ -291,9 +291,9 @@
 
 (defun user-config/sandbox-nix (command)
   "Sandbox a command inside nix-shell if required"
-  (let ((nix-file "shell.nix")))
-  (let ((nix-path "NIX_PATH=\"nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos/nixpkgs\"")))
-  (let ((nix-file-directory (locate-dominating-file default-directory nix-file))))
-  (when nix-file-directory
-    (let (nix-shell-path (expand-file-name nix-file nix-file-directory))
-      string-join '(nix-path "nix-shell -f" nix-shell-path "--command" "\"" command "\"") " ")))
+  (let ((nix-file "shell.nix"))
+    (let ((nix-path "NIX_PATH=\"nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos/nixpkgs\""))
+      (let ((nix-file-directory (locate-dominating-file default-directory nix-file)))
+        (when nix-file-directory
+          (let (nix-shell-path (expand-file-name nix-file nix-file-directory))
+            string-join '(nix-path "nix-shell -f" nix-shell-path "--command" "\"" command "\"") " "))))))
